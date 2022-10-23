91dd206be1fbaae613df4d7271f036a13d4650f6
Асель <a.a.asel2601@gmail.com>
2022-10-23 16:46:05 +0600

    lab9

---
+++ main.js
@@ -0,0 +1,22 @@
+//First problem (#4)//
+let username = 'Asel'
+console.log("Hi, " + username +"!")
+
+
+//Second problem(#13)//
+let number = 70
+if (number>0) {
+  console.log(number + " is positive.")
+} else {
+  console.log(number + " is negative.")
+}
+
+
+//Third problem(#9)//
+let number1 = 7
+let number2 = -15
+if (number1 > number2) {
+  console.log(number1 + " is greater.")
+} else {
+    console.log(number2 + " is greater.")
+}
